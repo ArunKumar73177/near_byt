142ea6c88a9ca0d920aea9bf4615d8e7fe4530bd
ArunKumar73177 <arunsharma73177@gmail.com>
2025-11-05 20:00:25 +0530

    update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+// Assuming LoginScreen is the name of the widget in this file
 import 'login_page.dart';
+// We need to import the main page as well
+import 'main_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,7 +21,23 @@ class MyApp extends StatelessWidget {
         useMaterial3: false,
         scaffoldBackgroundColor: const Color(0xFFE8E9F3),
       ),
-      home: const LoginScreen(),
+
+      // 1. Set the initial route to the login screen
+      initialRoute: '/login',
+
+      // 2. Define all the application routes
+      routes: {
+        // Route for the login screen
+        '/login': (context) => const LoginScreen(),
+        // Route for the main page (assuming the widget is called MainPage)
+        '/main': (context) => const MainPage(),
+      },
+
+      // Removed: home: const LoginScreen(),
     );
   }
 }
+
+// NOTE: You will need to make sure the MainPage widget exists in main_page.dart
+// Example:
+// class MainPage extends StatelessWidget { ... }
